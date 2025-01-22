0e7e703773c1ebed4f5a8f7901a440c2a6c8ac47
rijan <rijan.majhi@bookmundi.com>
2025-01-22 14:43:43 +0545

    fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,32 +1,32 @@
-import Header from "@/components/header/header";
-import AboutProjectSection from "@/components/projects/about-project-section";
-import ProjectBanner from "@/components/projects/project-banner";
-import ProjectBannerSection from "@/components/projects/project-banner-section";
-import BottomBanner from "@/components/projects/Project-bottom-banner";
-import ProjectSection from "@/components/projects/project-section";
-import WhyProjectSection from "@/components/projects/why-project-section";
+// import Header from "@/components/header/header";
+// import AboutProjectSection from "@/components/projects/about-project-section";
+// import ProjectBanner from "@/components/projects/project-banner";
+// import ProjectBannerSection from "@/components/projects/project-banner-section";
+// import BottomBanner from "@/components/projects/Project-bottom-banner";
+// import ProjectSection from "@/components/projects/project-section";
+// import WhyProjectSection from "@/components/projects/why-project-section";
 
-export const metadata = {
-  title: "Portfolio | Projects",
-  description: "Showcasing my projects.",
-};
+// export const metadata = {
+//   title: "Portfolio | Projects",
+//   description: "Showcasing my projects.",
+// };
 
-const ProjectPage = () => {
-  return (
-    <section className="mx-auto">
-      <Header />
-      <>
-        <div className="flex flex-col md:pb-72">
-          <ProjectBanner />
-          <ProjectBannerSection />
-        </div>
-        <WhyProjectSection />
-        <ProjectSection />
-        <AboutProjectSection />
-        <BottomBanner />
-      </>
-    </section>
-  );
-};
+// const ProjectPage = () => {
+//   return (
+//     <section className="mx-auto">
+//       <Header />
+//       <>
+//         <div className="flex flex-col md:pb-72">
+//           <ProjectBanner />
+//           <ProjectBannerSection />
+//         </div>
+//         <WhyProjectSection />
+//         <ProjectSection />
+//         <AboutProjectSection />
+//         <BottomBanner />
+//       </>
+//     </section>
+//   );
+// };
 
-export default ProjectPage;
+// export default ProjectPage;
